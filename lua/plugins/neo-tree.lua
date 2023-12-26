4920ca6cd7edec23d57e46a2b00cc1755d1ff2ab
BIPPAN <bippan1407@gmail.com>
2023-12-26 20:11:06 +0530

    telescope changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   keys = {
     {
       "<leader>e",
@@ -19,6 +20,16 @@ return {
     {
       "<leader>fe",
       false,
+    },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
     },
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {
-  "nvim-telescope/telescope.nvim",
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
   keys = {
     {
       "<leader>/",
@@ -15,5 +18,97 @@ return {
     },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader>,", false },
+    {
+      "fr",
+      function()
+        local telescope = require("telescope")
+        telescope.extensions.file_browser.file_browser({
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
+      end,
+    },
+    {
+      "ff",
+      function()
+        local telescope = require("telescope")
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+        telescope.extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
+      end,
+    },
   },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+    opts.picker = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
+      },
+    }
+
+    opts.extensions = {
+      file_browser = {
+        theme = "dropdown",
+        hijack_newtr = true,
+        mappings = {
+          ["n"] = {
+            ["n"] = fb_actions.create,
+            ["d"] = fb_actions.remove,
+            ["m"] = fb_actions.move,
+            ["r"] = fb_actions.rename,
+            ["h"] = fb_actions.goto_parent_dir,
+            ["/"] = function()
+              vim.cmd("startinsert")
+            end,
+            ["C-u"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_previous(prompt_bufnr)
+              end
+            end,
+            ["C-d"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_next(prompt_bufnr)
+              end
+            end,
+            ["PageUp"] = actions.preview_scrolling_up,
+            ["PageDown"] = actions.preview_scrolling_down,
+          },
+        },
+      },
+    }
+    telescope.setup(opts)
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("file_browser")
+  end,
 }
