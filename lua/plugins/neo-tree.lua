71f60660aec2e0aa03a98d051d80b3b746e8a616
BIPPAN <bippan1407@gmail.com>
2024-12-08 21:45:18 +0530

    add configurations

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree",
+      },
+      {
+        "<leader>fE",
+        false,
+      },
+      {
+        "<leader>E",
+        false,
+      },
+      {
+        "<leader>fe",
+        false,
+      },
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+        follow_current_file = { enabled = true },
+      },
+    },
+  }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,157 @@
+return {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+    keys = {
+      {
+        "<leader>/",
+        false,
+      },
+      {
+        "<leader><space>",
+        false,
+      },
+      {
+        "<leader>fF",
+        false,
+      },
+      { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
+      { "<leader>,", false },
+      {
+        "fr",
+        function()
+          local telescope = require("telescope")
+          -- telescope.setup({
+          --   defaults = {
+          --     -- hidden = true,
+          --     file_ignore_patterns = {
+          --       "node_modules",
+          --       "build",
+          --       "dist",
+          --       "yarn.lock",
+          --       ".git",
+          --       ".yarn",
+          --       "storybook-static",
+          --       ".gitignore",
+          --     },
+          --   },
+          -- })
+          telescope.extensions.file_browser.file_browser({
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+      },
+      {
+        "ff",
+        function()
+          local telescope = require("telescope")
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+      },
+      {
+        "<leader>gc",
+        "<cmd>Telescope git_branches<cr>",
+        desc = "Git branches",
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local fb_actions = require("telescope").extensions.file_browser.actions
+  
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+        wrap_results = true,
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          n = {},
+        },
+        hidden = true,
+        file_ignore_patterns = {
+          "node_modules",
+          "build",
+          "dist",
+          "yarn.lock",
+          ".git",
+          ".yarn",
+          "storybook-static",
+          ".gitignore",
+        },
+      })
+      opts.pickers = {
+        diagnostics = {
+          theme = "ivy",
+          initial_mode = "normal",
+          layout_config = {
+            preview_cutoff = 9999,
+          },
+        },
+        git_branches = {
+          mappings = {
+            ["i"] = { ["<cr>"] = actions.git_switch_branch },
+          },
+        },
+      }
+  
+      opts.extensions = {
+        file_browser = {
+          theme = "dropdown",
+          hijack_newtr = true,
+          mappings = {
+            ["n"] = {
+              ["n"] = fb_actions.create,
+              ["d"] = fb_actions.remove,
+              ["m"] = fb_actions.move,
+              ["r"] = fb_actions.rename,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["c-u"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["c-d"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+              ["pageup"] = actions.preview_scrolling_up,
+              ["pagedown"] = actions.preview_scrolling_down,
+              ["cd"] = function(prompt_bufnr)
+                local selection = require("telescope.actions.state").get_selected_entry()
+                local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+                require("telescope.actions").close(prompt_bufnr)
+                -- Depending on what you want put `cd`, `lcd`, `tcd`
+                vim.cmd(string.format("silent cd %s", dir))
+              end
+            },
+          },
+        },
+      }
+      telescope.setup(opts)
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("file_browser")
+    end,
+  }
